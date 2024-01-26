9e2301279d0f3033b0dae48ee43a9a3e0a343969
Alexin <alexindevs@gmail.com>
2024-01-26 22:59:11 +0100

    Add initial file service

--- src/models/file.repository.ts
+++ src/models/file.repository.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, File, Folder } from "@prisma/client";
+import logger from "../utils/logging/logger";
 
 const prisma = new PrismaClient();
 
@@ -19,8 +20,17 @@ export default class FileRepository {
             });
             return file;
         } catch (error) {
+            logger.error("Error adding file: ", error);
             throw error;
         }
+    }
+
+    async getFileMetadata(id: number) {
+        return prisma.file.findUnique({
+            where: {
+                id
+            }
+        });
     }
 
     async getFilesByUser(userId: number) {
@@ -55,6 +65,38 @@ export default class FileRepository {
         }
     }
 
+    async removeFileFromFolder(folderId: number, fileId: number) {
+        try {
+            const file = await prisma.file.update({
+                where: {
+                    id: fileId
+                },
+                data: {
+                    folderId: null
+                }
+            });
+            return file;
+        } catch (error) {
+            logger.error("Error removing file from folder: ", error);
+            throw error;
+        }
+    }
+
+    async addFileToFolder(folderId: number, fileId: number) {
+        return prisma.folder.update({
+            where: {
+                id: folderId
+            },
+            data: {
+                files: {
+                    connect: {
+                        id: fileId
+                    }
+                }
+            }
+        });
+    }
+
     async getFoldersByUserId(userId: number) {
         return prisma.folder.findMany({
             where: {
@@ -72,10 +114,38 @@ export default class FileRepository {
     }
 
     async deleteFolder (id: number) {
-        // TODO:  Check if folder has any files, delete files if it does
+        const filesInFolder = await prisma.file.findMany({
+            where: {
+                folderId: id
+            }
+        })
+        if (filesInFolder.length > 0) {
+            return prisma.file.deleteMany({
+                where: {
+                    folderId: id
+                }
+            });
+        }
         return prisma.folder.delete({
             where: {
                 id
+            }
+        });
+    }
+
+    /**
+     * Asynchronously retrieves a folder and its associated files by ID.
+     *
+     * @param {number} id - The ID of the folder to retrieve.
+     * @return {Promise<Folder>} The folder and its associated files.
+     */
+    async getFolderAndFiles(id: number) {
+        return prisma.folder.findUnique({
+            where: {
+                id
+            },
+            include: {
+                files: true
             }
         });
     }
---
+++ src/services/files.service.ts
@@ -0,0 +1,106 @@
+import FileRepository from "../models/file.repository";
+import logger from "../utils/logging/logger";
+
+const fileRepository = new FileRepository();
+
+class FileService {
+    async addFile (userId: number, name: string, path: string, size: number, type: string, extension: string, metadata: string, hash: string) {
+        try {
+            const file = await fileRepository.addFile(userId, name, path, size, type, extension, metadata, hash);
+            return file;
+        } catch (error) {
+            logger.error("Error adding file: ", error);
+            throw error;
+        }
+    }
+
+    async addFolder (userId: number, name: string, path: string) {
+        try {
+            const folder = await fileRepository.addFolder(userId, name, path);
+            return folder;
+        } catch (error) {
+            logger.error("Error adding folder: ", error);
+            throw error;
+        }
+    }
+
+    async removeFileFromFolder(folderId: number, fileId: number) {
+        try {
+            const file = await fileRepository.removeFileFromFolder(folderId, fileId);
+            return file;
+        } catch (error) {
+            logger.error("Error removing file from folder: ", error);
+            throw error;
+        }
+    }
+
+    async addFileToFolder(folderId: number, fileId: number) {
+        try {
+            const file = await fileRepository.addFileToFolder(folderId, fileId);
+            return file;
+        } catch (error) {
+            logger.error("Error adding file to folder: ", error);
+            throw error;
+        }
+    }
+
+    async getFilesByUser(userId: number) {
+        try {
+            const files = await fileRepository.getFilesByUser(userId);
+            return files;
+        } catch (error) {
+            logger.error("Error getting files by user: ", error);
+            throw error;
+        }
+    }
+
+    async getFilesInFolder(userId: number, folderId: number) {
+        try {
+            const files = await fileRepository.getFilesInFolder(userId, folderId);
+            return files;
+        } catch (error) {
+            logger.error("Error getting files in folder: ", error);
+            throw error;
+        }
+    }
+
+    async getFileMetadata(id: number) {
+        try {
+            const file = await fileRepository.getFileMetadata(id);
+            return file;
+        } catch (error) {
+            logger.error("Error getting file metadata: ", error);
+            throw error;
+        }
+    }
+
+    async getFolderAndFiles(id: number) {
+        try {
+            const folderAndFiles = await fileRepository.getFolderAndFiles(id);
+            return folderAndFiles;
+        } catch (error) {
+            logger.error("Error getting folder and files: ", error);
+            throw error;
+        }
+    }
+
+    async deleteFile(id: number) {
+        try {
+            const file = await fileRepository.deleteFile(id);
+            return file;
+        } catch (error) {
+            logger.error("Error deleting file: ", error);
+            throw error;
+        }
+    }
+
+    async deleteFolder(id: number) {
+        try {
+            const folder = await fileRepository.deleteFolder(id);
+            return folder;
+        } catch (error) {
+            logger.error("Error deleting folder: ", error);
+            throw error;
+        }
+    }
+}
